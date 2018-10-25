e4a5251b1d8fb3b239e08326108274971521a646
David Lee <javaugi@yahoo.com>
2018-10-24 23:06:51 -0400

    added CI examples

---
+++ src/main/java/com/sisllc/mathformulas/ci/ch03/Q31FullStackException.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2018 Center for Information Management, Inc.
+ *
+ * This program is proprietary.
+ * Redistribution without permission is strictly prohibited.
+ * For more information, contact <http://www.ciminc.com>
+ */
+package com.sisllc.mathformulas.ci.ch03;
+
+/**
+ *
+ *
+ * @author javaugi
+ * @version $LastChangedRevision $LastChangedDate Last Modified Author:
+ * $LastChangedBy
+ */
+public class Q31FullStackException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public Q31FullStackException() {
+        super();
+    }
+
+    public Q31FullStackException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/sisllc/mathformulas/ci/ch03/Q31StackData.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2018 Center for Information Management, Inc.
+ *
+ * This program is proprietary.
+ * Redistribution without permission is strictly prohibited.
+ * For more information, contact <http://www.ciminc.com>
+ */
+package com.sisllc.mathformulas.ci.ch03;
+
+/**
+ *
+ *
+ * @author javaugi
+ * @version $LastChangedRevision $LastChangedDate Last Modified Author:
+ * $LastChangedBy
+ */
+public class Q31StackData {
+
+    public int start;
+    public int pointer;
+    public int size = 0;
+    public int capacity;
+
+    public Q31StackData(int _start, int _capacity) {
+        start = _start;
+        pointer = _start - 1;
+        capacity = _capacity;
+    }
+
+    public boolean isWithinStack(int index, int total_size) {
+        // Note: if stack wraps, the head (right side) wraps around to the left.
+        if (start <= index && index < start + capacity) {
+            // non-wrapping, or "head" (right side) of wrapping case
+            return true;
+        } else if (start + capacity > total_size
+                && index < (start + capacity) % total_size) {
+            // tail (left side) of wrapping case
+            return true;
+        }
+        return false;
+    }
+}
