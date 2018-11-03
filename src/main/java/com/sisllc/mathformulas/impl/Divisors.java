adc59d930fb81078971f27e5a9e4a0bc677a10e0
David Lee <javaugi@yahoo.com>
2018-11-03 15:55:58 -0400

    added lcm and gcd

--- src/main/java/com/sisllc/mathformulas/impl/Divisors.java
+++ src/main/java/com/sisllc/mathformulas/impl/Divisors.java
@@ -1,5 +1,8 @@
 package com.sisllc.mathformulas.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * List Of Divisors A divisor is an integer that can be multiplied by some other
  * integer to produce n. The following java program list all of the divisors of
@@ -8,10 +11,28 @@ package com.sisllc.mathformulas.impl;
  * @author david
  *
  */
+// 1: 1
+// 2: 1 2
+// 3: 1 3
+// 4: 1 2 4
+// 5: 1 5
+// 6: 1 2 3 6
+// 7: 1 7
+// 8: 1 2 4 8
+// 9: 1 3 9
+// 10: 1 2 5 10
+// 11: 1 11
+// 12: 1 2 3 4 6 12
+// 13: 1 13
+// 14: 1 2 7 14
+// 15: 1 3 5 15
+// 25: 1 5 5 25
 public class Divisors {
 
     public static void main(String[] args) {
-        int N = 15; // Integer.parseInt(args[0]);
+        int N = 25; // Integer.parseInt(args[0]);
+
+        System.out.println("" + getDivisors2(N));
 
         for (int i = 1; i <= N; i++) {
             System.out.print(i + ": ");
@@ -20,7 +41,24 @@ public class Divisors {
                     System.out.print(j + " ");
                 }
             }
-            System.out.println("");
+            System.out.println("\n " + getDivisors2(i));
         }
+    }
+
+    public static List<Integer> getDivisors(int n) {
+        List<Integer> rv = new ArrayList<>();
+
+        return rv;
+    }
+
+    public static List<Integer> getDivisors2(int n) {
+        List<Integer> rv = new ArrayList<>();
+
+        for (int j = 1; j <= n; j++) {
+            if (n % j == 0) {
+                rv.add(j);
+            }
+        }
+        return rv;
     }
 }
