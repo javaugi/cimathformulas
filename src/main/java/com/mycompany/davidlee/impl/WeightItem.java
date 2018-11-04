7498560797991f556a5c0802dfd71fd6eaddeae4
David Lee <javaugi@yahoo.com>
2018-11-04 11:26:21 -0500

    merge shopping app

---
+++ src/main/java/com/mycompany/davidlee/impl/WeightItem.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2018 Center for Information Management, Inc.
+ *
+ * This program is proprietary.
+ * Redistribution without permission is strictly prohibited.
+ * For more information, contact <http://www.ciminc.com>
+ */
+package com.mycompany.davidlee.impl;
+
+
+
+/**
+ *
+ *
+ * @author javaugi
+ * @version $LastChangedRevision $LastChangedDate Last Modified Author:
+ * $LastChangedBy
+ */
+public class WeightItem extends ItemImpl {
+
+    private double weightInPound;
+    private double pricePerPound;
+
+    public WeightItem(String name, double weightInPound, double pricePerPound) {
+        super(name);
+        this.weightInPound = weightInPound;
+        this.pricePerPound = pricePerPound;
+        super.setCategory(Category.Weight);
+        super.setPrice(weightInPound * pricePerPound);
+    }
+
+    public double getWeightInPound() {
+        return weightInPound;
+    }
+
+    public void setWeightInPound(double weightInPound) {
+        this.weightInPound = weightInPound;
+    }
+
+    public double getPricePerPound() {
+        return pricePerPound;
+    }
+
+    public void setPricePerPound(double pricePerPound) {
+        this.pricePerPound = pricePerPound;
+    }
+
+}
