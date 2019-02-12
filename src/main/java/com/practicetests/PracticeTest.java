5c59f17236290dbbb1c673d02fae47425729f9d9
David Lee <david@ciminc.com>
2019-02-12 17:27:53 -0500

    practice test link

---
+++ src/main/java/com/practicetests/PracticeTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2019 Center for Information Management, Inc.
+ *
+ * This program is proprietary.
+ * Redistribution without permission is strictly prohibited.
+ * For more information, contact <http://www.ciminc.com>
+ */
+package com.practicetests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * https://www.wisdomjobs.com/e-university/java-multithreading-practice-tests-862-327443-10350dfa459b5f67ac6760a5d134e02df9ca2
+ *
+ * @author david
+ * @version $LastChangedRevision $LastChangedDate Last Modified Author:
+ * $LastChangedBy
+ */
+public class PracticeTest {
+
+    private static final Logger log = LoggerFactory.getLogger(PracticeTest.class);
+}
