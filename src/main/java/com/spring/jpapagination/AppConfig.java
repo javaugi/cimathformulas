98f817943e92c41186bf4d692c6a40f69da4f50d
David Lee <javaugi@yahoo.com>
2020-06-25 21:56:34 -0400

    updated for boot 2.3.1

--- src/main/java/com/spring/jpapagination/AppConfig.java
+++ src/main/java/com/spring/jpapagination/AppConfig.java
@@ -13,11 +13,18 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import javax.persistence.EntityManagerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 @EnableJpaRepositories
 @ComponentScan
 @Configuration
 public class AppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
