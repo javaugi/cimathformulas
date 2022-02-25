3f4e1363acbe44006aaf81e77e0e7895bbcfe4f5
javaugi <30354702+javaugi@users.noreply.github.com>
2022-02-25 16:33:54 -0500

    updated Fibonacci impl

--- src/main/java/com/fibonacci/FibonacciRecursion.java
+++ src/main/java/com/fibonacci/FibonacciRecursion.java
@@ -12,9 +12,9 @@ public class FibonacciRecursion {
     private static long fib(int n) {
         if (n <= 1) {
             return n;
-        } else {
-            return fib(n - 1) + fib(n - 2);
         }
+        
+        return fib(n - 1) + fib(n - 2);
     }
 
     private static void printFib(int n) {
