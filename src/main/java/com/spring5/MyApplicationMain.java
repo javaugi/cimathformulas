99e460cdb3d34266f92ca2a9b462b3b0f3e9844b
David Lee <javaugi@yahoo.com>
2025-04-20 23:20:23 -0400

    added neo4j service and controller

--- src/main/java/com/spring5/MyApplicationMain.java
+++ src/main/java/com/spring5/MyApplicationMain.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 
@@ -21,7 +22,7 @@ import redis.clients.jedis.Jedis;
  *
  * @author javaugi
  */
-@Service
+@Component
 public class MyApplicationMain {
     
     @Value("${app.mongodb.enabled}")
